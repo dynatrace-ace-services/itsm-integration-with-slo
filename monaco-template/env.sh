fbc9fce6258726e500fe87caeb4df09ea742d3a8
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2023-03-14 07:53:31 +0100

    Update env.sh

--- monaco-template/env.sh
+++ monaco-template/env.sh
@@ -12,22 +12,20 @@
   #export slo_prefix="my_appli" #no space, no "-", no special character, only "az09_"
 
 #default application-performance configuration
-  export application_performance_target=85
+  export application_performance_target=85 #for apdex = 0.85,minimum value of a good apdex
   export application_performance_warning=90
-  export application_performance_burnrate=10
+  export application_performance_burnrate="3,5"
   export application_performance_period="-1w"
-
+  
 #default frontendservice_availability configuration
   export frontendservice_availability_target=95
   export frontendservice_availability_warning=98
-  export frontendservice_availability_burnrate=10
+  export frontendservice_availability_burnrate="3,5"
   export frontendservice_availability_period="-1w"
 
 #default metric events
   export application_performance_fastburnalert_enable="true"
-  export application_performance_errorbudgetalert_enable="false"  #validate the apdex > 0.9 before enabling this alert
   export frontendservice_availability_fastburnalert_enable="true"
-  export frontendservice_availability_errorbudgetalert_enable="true"
 
 #alerting profile
   export delay_for_real_time_alert=0 #minutes
